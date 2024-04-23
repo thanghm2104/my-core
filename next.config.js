2f8e2fda7593c2b05073ab4fc458e1a9ed9332a2
thanghm5 <v.thanghm5@vinhomes.vn>
2024-04-23 11:43:37 +0700

    Update database configuration in next.config.js

--- next.config.js
+++ next.config.js
@@ -1,21 +1,23 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    reactStrictMode: true,
-    serverRuntimeConfig: {
-        dbConfig: {
-            host: 'localhost',
-            port: 3306,
-            user: 'root',
-            password: '2204', // @@@
-            database: 'next-js-registration-login-example'
-        },
-        secret: 'THIS IS USED TO SIGN AND VERIFY JWT TOKENS, REPLACE IT WITH YOUR OWN SECRET, IT CAN BE ANY STRING'
+  reactStrictMode: true,
+  serverRuntimeConfig: {
+    dbConfig: {
+      host: "localhost",
+      port: 3306,
+      user: "admin",
+      password: "Abc1234@", // @@@
+      database: "idea",
     },
-    publicRuntimeConfig: {
-        apiUrl: process.env.NODE_ENV === 'development'
-            ? 'http://localhost:3000/api' // development api
-            : 'http://localhost:3000/api' // production api
-    }
-}
+    secret:
+      "rp+Q/4NyXr42T+Q8/nQczQAQuoCk+XND9eoIycIO2VlrDzgZJeOqhf5YTbad7KEXGDh3YgSkns/nDby+4H0GRCu7RUBK+Av5+0UQk2rjq+VZIEujGCLS7ATTz2KnDqeHBOfoRxCeDNHN5Tjf3uxwSDD/Z3MhPuQdWn/P0qC1otFMyrgGPDmcG4NgEG4JhfSinDuUSxHSqZy7jHTbVVHPC4bn/JsU3YcnSSd3Npff29i6E6mNssdl8xJzpMlw/K5QC1eoTQ0Ou4GBT6hnI5x/JQ1zFc+E9xfag8ErNTwiPr2Gaj+B1LzHigr+pyZE9oaYhC3mKtFMmKNr3X7YUt7/OQ==",
+  },
+  publicRuntimeConfig: {
+    apiUrl:
+      process.env.NODE_ENV === "development"
+        ? "http://localhost:3000/api" // development api
+        : "http://localhost:3000/api", // production api
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
