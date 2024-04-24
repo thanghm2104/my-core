8f91ba284be3d63d3f84b645ada59b7cc15bdae1
thanghm5 <v.thanghm5@vinhomes.vn>
2024-04-24 15:40:26 +0700

    Add winston logger to fetch-wrapper.js

--- helpers/fetch-wrapper.js
+++ helpers/fetch-wrapper.js
@@ -1,59 +1,79 @@
-import getConfig from 'next/config';
+import getConfig from "next/config";
 
-import { userService } from 'services';
+import { userService } from "services";
 
 const { publicRuntimeConfig } = getConfig();
+import winston from "winston";
 
+export const winstonLogger = winston.createLogger({
+  level: "info", // Log only info level and above
+  format: winston.format.json(),
+  transports: [
+    new winston.transports.File({ filename: "error.log", level: "error" }), // Log only errors
+    new winston.transports.File({ filename: "combined.log" }), // Log all levels
+  ],
+});
+
+if (process.env.NODE_ENV !== "production") {
+  logger.add(
+    new winston.transports.Console({
+      format: winston.format.simple(),
+    })
+  );
+}
 export const fetchWrapper = {
-    get: request('GET'),
-    post: request('POST'),
-    put: request('PUT'),
-    delete: request('DELETE')
+  get: request("GET"),
+  post: request("POST"),
+  put: request("PUT"),
+  delete: request("DELETE"),
 };
 
 function request(method) {
-    return (url, body) => {
-        const requestOptions = {
-            method,
-            headers: authHeader(url)
-        };
-        if (body) {
-            requestOptions.headers['Content-Type'] = 'application/json';
-            requestOptions.body = JSON.stringify(body);
-        }
-        return fetch(url, requestOptions).then(handleResponse);
+  return (url, body) => {
+    const requestOptions = {
+      method,
+      headers: authHeader(url),
+    };
+    if (body) {
+      requestOptions.headers["Content-Type"] = "application/json";
+      requestOptions.body = JSON.stringify(body);
     }
+    winstonLogger.info("Request URL: ", url);
+    return fetch(url, requestOptions).then(handleResponse);
+  };
 }
 
 // helper functions
 
 function authHeader(url) {
-    // return auth header with jwt if user is logged in and request is to the api url
-    const user = userService.userValue;
-    const isLoggedIn = user?.token;
-    const isApiUrl = url.startsWith(publicRuntimeConfig.apiUrl);
-    if (isLoggedIn && isApiUrl) {
-        return { Authorization: `Bearer ${user.token}` };
-    } else {
-        return {};
-    }
+  // return auth header with jwt if user is logged in and request is to the api url
+  const user = userService.userValue;
+  const isLoggedIn = user?.token;
+  const isApiUrl = url.startsWith(publicRuntimeConfig.apiUrl);
+  if (isLoggedIn && isApiUrl) {
+    return { Authorization: `Bearer ${user.token}` };
+  } else {
+    return {};
+  }
 }
 
 async function handleResponse(response) {
-    const isJson = response.headers?.get('content-type')?.includes('application/json');
-    const data = isJson ? await response.json() : null;
-
-    // check for error response
-    if (!response.ok) {
-        if ([401, 403].includes(response.status) && userService.userValue) {
-            // auto logout if 401 Unauthorized or 403 Forbidden response returned from api
-            userService.logout();
-        }
-
-        // get error message from body or default to response status
-        const error = (data && data.message) || response.statusText;
-        return Promise.reject(error);
+  const isJson = response.headers?.get("content-type")?.includes("application/json");
+  const data = isJson ? await response.json() : null;
+  // Log the response data
+  winstonLogger.info(data);
+  // check for error response
+  if (!response.ok) {
+    if ([401, 403].includes(response.status) && userService.userValue) {
+      // auto logout if 401 Unauthorized or 403 Forbidden response returned from api
+      winstonLogger.error("Unauthorized or Forbidden response returned from api");
+      userService.logout();
     }
+    winstonLogger.error("Error response returned from api:", error);
+    // get error message from body or default to response status
+    const error = (data && data.message) || response.statusText;
+    return Promise.reject(error);
+  }
 
-    return data;
+  return data;
 }
